a52afbc71e4697c54b42ab816cbd9fcb25893270
Kumari Anjali Rao <118367818+anjalirao2001@users.noreply.github.com>
2024-04-25 23:37:50 +0530

    added date in php

---
+++ 13-date.php
@@ -0,0 +1,42 @@
+<?php
+echo date("y/m/d");
+echo "<br>";
+
+echo date("Y/m/d");
+echo "<br>";
+
+echo date("Y-m-d");
+echo "<br>";
+
+echo date("Y.m.d");
+echo "<br>";
+
+echo date("Y");
+echo "<br>";
+
+echo date("y");
+echo "<br>";
+
+echo date("m");
+echo "<br>";
+
+echo date("M");
+echo "<br>";
+
+echo date("d");
+echo "<br>";
+
+echo date("D");
+echo "<br>";
+
+echo date("l");
+echo "<br>";
+
+echo date("L");
+echo "<br>";
+
+
+echo date("h:i:sa"); //date and time in 12 hour format
+?>
+
+<p>Copyright 2010 - <?php echo date("Y")  ?></p>
