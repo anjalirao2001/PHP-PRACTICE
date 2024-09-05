4a4abfcc0de454304c921aa4bcc4c397453cf21f
Kumari Anjali Rao <118367818+anjalirao2001@users.noreply.github.com>
2024-09-05 17:26:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> include file</title>
+</head>
+<body>
+   
+   <footer>
+    <p>Copyright 2010 - <?php echo date("Y") ?></p>
+   </footer>
+</body>
+</html>
