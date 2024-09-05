3fea6cead7da2e1afc17e180616563b5c49783e3
Kumari Anjali Rao <118367818+anjalirao2001@users.noreply.github.com>
2024-09-05 17:26:28 +0530

    added about in php

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+echo "About Page";
+
+// in include if the file is not present then it will not show error
+// include "header.php";
+// include "footer.php";
+
+// in require if the file is not present then it will not execute & show error
+require "header.php";
+require "footer.php";
+
+?>
+
+
