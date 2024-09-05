b27764110d34d09d3f7ece592f6e39ad2e8fdac3
Kumari Anjali Rao <118367818+anjalirao2001@users.noreply.github.com>
2024-09-05 17:27:04 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP include file</title>
+</head>
+<body>
+   
+    <header>
+        <!-- <h1>Header Section</h1> -->
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="features.php">Features</a></li>
+        </ul>
+    </header>
+</body>
+</html>
